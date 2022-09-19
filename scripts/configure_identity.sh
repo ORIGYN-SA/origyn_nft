e80e714f8a5f858d1b63873c8bb37aaa564a6042
Ivan Chernukha <ivan.chernukha@origyn.ch>
2022-09-19 16:31:00 +0300

    updated test runner

--- scripts/configure_identity.sh
+++ scripts/configure_identity.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -ex
 
-dfx identity new default --disable-encryption || true
+# dfx identity new default --disable-encryption || true
 
-mkdir ~/.config/dfx/identity/test_nft_ref/
-# cp identity.pem ~/.config/dfx/identity/test_nft_ref/identity.pem
-dfx identity use test_nft_ref
-dfx identity whoami
+# mkdir ~/.config/dfx/identity/test_nft_ref/
+# # cp identity.pem ~/.config/dfx/identity/test_nft_ref/identity.pem
+# dfx identity use test_nft_ref
+# dfx identity whoami
